d5575701a90b073697f83ee2ee2d8c9e0d062156
Chris Elford <chris.n.elford@gmail.com>
2019-04-16 21:51:13 +0100

    First basic guessing game
    The game picks a pitch, plays it in the middle octave, then asks the user what it was.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,24 @@
 module Lib
-    ( someFunc
+    ( runQuiz
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Random
+import Euterpea
+
+randomPitchClass :: (RandomGen g) => g -> (PitchClass, g)
+randomPitchClass g = (toEnum randomInt, g')
+  where minPitchClass = minBound :: PitchClass
+        maxPitchClass = maxBound :: PitchClass
+        (randomInt, g') = randomR (fromEnum minPitchClass, fromEnum maxPitchClass) g
+
+runQuiz :: IO ()
+runQuiz = do
+    gen <- getStdGen
+    let (pitch, _) = randomPitchClass gen
+    play $ note 1 (pitch, 4 :: Octave)
+    putStrLn "What note did you hear?"
+    answer <- getLine
+    let answerPitch = read answer :: PitchClass
+    if absPitch (answerPitch, 4) == absPitch (pitch, 4)
+        then putStrLn "Correct"
+        else putStrLn $ "Wrong: that was a " ++ show pitch
