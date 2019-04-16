700d50f7224a49519d370251e40939ae06ae24fd
Chris Elford <chris.n.elford@gmail.com>
2019-04-16 22:18:42 +0100

    Add in interval-based quiz
    Ask user how many semitones between two notes.

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,7 +14,25 @@ randomOctave :: RandomGen g => g -> (Octave, g)
 randomOctave = randomR (-1, 9)
 
 runQuiz :: IO ()
-runQuiz = do
+runQuiz = intervalQuiz
+
+intervalQuiz :: IO ()
+intervalQuiz = do
+    gen <- getStdGen
+    let (pitch, gen') = random gen :: (PitchClass, StdGen)
+    let (octave, gen'') = randomOctave gen'
+    let (interval, _) = randomR (1, 12) gen''
+    let firstNote = note (1/4) (pitch, octave)
+    play $ firstNote :+: rest (1/8) :+: transpose interval firstNote
+    putStrLn "How many semitones?"
+    answer <- getLine
+    let answerInterval = read answer :: Int
+    if answerInterval == interval
+        then putStrLn "Correct"
+        else putStrLn $ "Wrong: that was " ++ show interval ++ " semitones."
+
+singleToneQuiz :: IO ()
+singleToneQuiz = do
     gen <- getStdGen
     let (pitch, gen') = random gen
     let (octave, _) = randomOctave gen'
