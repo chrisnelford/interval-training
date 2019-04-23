29beabd818dd5cee2e718051cf3bd042402896c1
Chris Elford <chris.n.elford@gmail.com>
2019-04-23 23:19:46 +0100

    More sensible random notes.
    We want a uniform distribution of all of the notes. Rather than
    mixing up different names for the same note, we follow Euterpea's
    lead and use sharps and naturals as the canonical pitch classes for
    each tone.

--- src/Random.hs
+++ src/Random.hs
@@ -11,9 +11,19 @@ import Control.Monad.Random
 import Euterpea
 
 -- Randomness for Euterpea note-level contstructs
+
+-- We want a uniform distribution of all of the notes. Rather than
+-- mixing up different names for the same note, we follow Euterpea's
+-- lead and use sharps and naturals as the canonical pitch classes for
+-- each tone.
+canonicalNotes :: [PitchClass]
+canonicalNotes = [C, Cs, D, Ds, E, F, Fs, G, Gs, A, As, B]
+
 instance Random PitchClass where
-    randomR (lower, upper) g = (toEnum randomInt, g')
-        where (randomInt, g') = randomR (fromEnum lower, fromEnum upper) g
+    randomR (lower, upper) g = runRand (fromList distribution) g
+        where distribution = weighted $ filter inRange canonicalNotes
+              inRange x = (x >= lower) && (x <= upper)
+              weighted = map (\x -> (x, 1))
     random = randomR (minBound, maxBound)
 
 randomOctave :: RandomGen g => Rand g Octave
